4f7507d60367011856cc6ed17dc031f8d14068f6
Hammed Akande <48578759+drhammed@users.noreply.github.com>
2024-07-10 08:29:48 -0400

    upd

--- main.py
+++ main.py
@@ -172,7 +172,7 @@ Please ensure this process is followed for all guidance and support calls.
             st.session_state.chat_history.append(("Chatbot", response))
             
             #clear the user input
-            st.session_state.user_input = ""
+            st.session_state["user_input"] = ""
             #st.experimental_rerun()
             st.rerun()
 
