daa2aaa152d3222dca1fd6bc119c8ed902c10446
Hammed Akande <48578759+drhammed@users.noreply.github.com>
2024-07-12 15:18:27 -0400

    upd

--- main.py
+++ main.py
@@ -44,8 +44,13 @@ st.write("Hello! I'm your friendly chatbot. I'm here to help answer your questio
 # Load environment variables from .env file
 load_dotenv()
 
+
 #OpenAI model
 OPENAI_API_KEY = st.secrets["api_keys"]["OPENAI_API_KEY"]
+
+# Initialize OpenAI model
+llm_mod = ChatOpenAI(model="gpt-4o", temperature=0, max_tokens=None, timeout=None, max_retries=2, api_key=OPENAI_API_KEY)
+
 
 # Initialize OpenAI model
 llm_mod = ChatOpenAI(model="gpt-4o", temperature=0, max_tokens=None, timeout=None, max_retries=2, api_key=OPENAI_API_KEY)
